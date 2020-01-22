dc986418c445188a822864ccbdd896e988518c80
daichi-iwamoto <da1_0410@ezweb.ne.jp>
2020-01-22 19:47:45 +0900

    タグごとに記事取得

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -2,6 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 
+// 指定のタグの記事のみ呼び出す
+function Tags(props) {
+  // 記事情報HTML格納変数
+  let posts = []
+
+  // 該当記事取得
+  props.data.map(( post ) => (
+    (post.node.frontmatter.tags.includes(props.tag)) ?
+      posts.push(
+        <Link className="blog-link" to={post.node.fields.slug}>
+          <article>
+            <div className="post-data">
+              <div className="post-head">
+                <p className="post-meta">
+                    {post.node.frontmatter.title}
+                </p>
+                <p className="post-date">
+                  {post.node.frontmatter.date}
+                </p>
+              </div>
+              <p className="post-description">
+                  {post.node.frontmatter.description}
+              </p>
+            </div>
+          </article>
+        </Link>
+      )
+      :
+      ""
+  ))
+
+  // 返り値
+  return (
+    <section>
+      <h3>{props.tag}</h3>
+      <div className="posts">
+        {posts}
+      </div>
+    </section>
+  ) 
+}
+
 class BlogRoll extends React.Component {
   render() {
     const { data } = this.props
@@ -33,6 +75,8 @@ class BlogRoll extends React.Component {
           ))
         }
         </div>
+        <Tags data={posts} tag={"test"} />
+        <Tags data={posts} tag={"code"} />
       </div>
     )
   }
@@ -65,6 +109,7 @@ export default () => (
                 templateKey
                 description
                 date(formatString: "YYYY/MM/DD")
+                tags
               }
             }
           }
