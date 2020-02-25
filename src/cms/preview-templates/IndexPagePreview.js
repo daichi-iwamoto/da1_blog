486f25dc21beb170fb635c1a6de0c8d97d3a2de9
daichi-iwamoto <da1_0410@ezweb.ne.jp>
2020-02-25 17:33:02 +0900

    ディレクトリ修正

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import { ProfilePageTemplate } from '../../templates/index-page'
 
-const IndexPagePreview = ({ entry, getAsset }) => {
+const ProfilePagePreview = ({ entry, widgetFor }) => {
   const data = entry.getIn(['data']).toJS()
 
-  if (data) {
-    return (
-      <IndexPageTemplate
-        image={data.image}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
+  return (
+    <ProfilePageTemplate
+      title={data.title}
+      content={widgetFor('body')}
+      skill={data.skill}
+    />
+  )
   }
-}
 
-IndexPagePreview.propTypes = {
+ProfilePagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 }
 
-export default IndexPagePreview
+export default ProfilePagePreview
