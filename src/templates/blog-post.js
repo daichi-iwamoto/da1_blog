b582f7b1def126ae297d3d1bf7a60dff697555e7
daichi_iwamoto <daichi.iwamoto@geo-code.co.jp>
2021-02-08 20:15:19 +0900

    fix canonical

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -72,7 +72,7 @@ const BlogPost = ({ data }) => {
               name="description"
               content={`${post.frontmatter.description}`}
             />
-            <link rel="canonical" href="https://noob-front-end-engineer-blog.com/typescript-npm-script/" />
+            <link rel="canonical" href="https://noob-front-end-engineer-blog.com/typescript-npm-scripts/" />
           </Helmet>
         }
         date={post.frontmatter.date}
